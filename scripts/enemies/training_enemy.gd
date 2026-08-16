722e63a6c8b2e65d40347d1c44f04f9ffd40ea1a
hallankazale <48835559+hallankazale@users.noreply.github.com>
2026-08-16 19:04:36 -0400

    Add lightweight training enemy logic

---
+++ scripts/enemies/training_enemy.gd
@@ -0,0 +1,18 @@
+extends Spatial
+
+export var max_health = 30
+export var xp_reward = 10
+
+var current_health = 0
+
+func _ready():
+    current_health = max_health
+    add_to_group("enemies")
+
+func take_damage(amount, attacker):
+    current_health -= amount
+
+    if current_health <= 0:
+        if attacker != null and attacker.has_method("add_xp"):
+            attacker.add_xp(xp_reward)
+        queue_free()
