3f1b67d09f58a61e33d7abfec4da25813a70fb12
hallankazale <48835559+hallankazale@users.noreply.github.com>
2026-08-16 19:04:56 -0400

    Add lightweight HUD for level and XP

---
+++ scripts/ui/hud.gd
@@ -0,0 +1,15 @@
+extends CanvasLayer
+
+onready var player = get_node("../Player")
+onready var stats_label = $MarginContainer/StatsLabel
+
+func _process(_delta):
+    if player == null:
+        return
+
+    stats_label.text = "Nivel %d  |  XP %d/%d  |  Ataque %d  |  ESPACO = atacar" % [
+        player.level,
+        player.xp,
+        player.xp_to_next_level,
+        player.attack_damage
+    ]
